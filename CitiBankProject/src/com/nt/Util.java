e7d1721bcd62e535832b9c15be6a53646db5428f
addagirinaveen <addagirinaveen@localhost>
2026-04-01 17:19:44 +0530

    conflit checking

--- CitiBankProject/src/com/nt/Util.java
+++ CitiBankProject/src/com/nt/Util.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
 	
 	System.out.println("hi");
 
-	System.out.println("code modified by dev-1 or tl");
+	System.out.println("code modified by dev-1 or team leader");
 
 	
 }
